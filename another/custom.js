dbdf127eb1748416040cf4c31d29baed933f54d9
DauMoe <47239428+DauMoe@users.noreply.github.com>
2020-02-14 18:55:54 +0700

    Update custom.js

--- another/custom.js
+++ another/custom.js
@@ -37,12 +37,7 @@ movebutton=()=>{
 	var y = Math.random()*height;
 	var x = Math.random()*width;
 	y = y/2+'px';
-	if (width<500) {
-		x = x/3 + 'px';
-	}
-	else {
-		x = x+'px';
-	}
+	x = x + 'px';
 	hide.style.top = x;
 	hide.style.left = y;
 }
